e8e5cb03994e9f7cfa0a44aec5532e3c43a0ba33
oraclespainpresales <oraclespainpresales@gmail.com>
2020-12-02 16:40:48 +0100

    new file:   .gitignore 	new file:   Dockerfile 	new file:   func.js 	new file:   func.yaml 	new file:   package-lock.json 	new file:   package.json

---
+++ func.js
@@ -0,0 +1,119 @@
+const oracledb = require('oracledb')
+    , fdk = require('@fnproject/fdk')
+    , moment = require('moment')
+;
+
+oracledb.outFormat = oracledb.OBJECT;
+oracledb.fetchAsString = [oracledb.CLOB];
+
+let pool;
+
+fdk.handle(async function(input){
+  let result = {};
+
+  if (!input.uuid) {
+    result.code = -1;
+    result.message = "UUID not provided";
+    return result;
+  }
+
+  if (!pool) {
+    pool = await oracledb.createPool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      connectString: process.env.CONNECT_STRING_MICROSERVICE,
+    });
+  }
+
+  const connection = await pool.getConnection();
+  const sql = `select * from polluuid where uuid = :uuid`;
+  const bindings = [input.uuid];
+  const records = await connection.execute(sql, bindings, { autoCommit: true });
+
+  // Let's start to validate all stuff
+  if (records.rows.length == 0) {
+    result.code = -1;
+    result.message = "Poll token not found";
+    return result;
+  }
+  
+  let r = records.rows[0];
+
+  if (r.USED !== "N") {
+    result.code = -1;
+    result.message = "This poll token has already been used";
+    return result;
+  }
+
+  let m1 = moment(r.DUEDATETIME);
+  let m2 = moment();
+  let diff = m1.diff(m2);
+  if (diff <= 0) {
+    result.code = -1;
+    result.message = "This poll token has expired";
+    result.expirationdate = m1.format('YYYY-MM-DDTHH:mm:ssZ');
+    return result;
+  }
+
+  result.code = 0;
+  result.orderid = r.ORDERID;
+  return result;
+})
+
+/**
+process.env.DB_USER = "microservice";
+process.env.DB_PASSWORD = "AAZZ__welcomedevops123";
+process.env.CONNECT_STRING_MICROSERVICE = "158.101.175.134:1521/dodbhp_pdb1.sub03010825490.devopsvcn.oraclevcn.com";
+
+async function x(input) {
+
+  let result = {};
+
+  if (!input.uuid) {
+    result.code = -1;
+    result.message = "UUID not provided";
+    return result;
+  }
+
+  if (!pool) {
+    pool = await oracledb.createPool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      connectString: process.env.CONNECT_STRING_MICROSERVICE,
+    });
+  }
+
+  const connection = await pool.getConnection();
+  const sql = `select * from polluuid where uuid = :uuid`;
+  const bindings = [input.uuid];
+  const records = await connection.execute(sql, bindings, { autoCommit: true });
+
+  // Let's start to validate all stuff
+  if (records.rows.length == 0) {
+    result.code = -1;
+    result.message = "Poll token not found";
+    return result;
+  }
+  
+  let r = records.rows[0];
+
+  if (r.USED !== "N") {
+    result.code = -1;
+    result.message = "This poll token has already been used";
+    return result;
+  }
+
+  let diff = moment().diff(moment(r.DUEDATETIME));
+  if (diff <= 0) {
+    result.code = -1;
+    result.message = "This poll token has expired";
+    return result;
+  }
+
+  result.code = 0;
+  return result;
+  
+}
+
+x({uuid: "3facc1587dfc4c9c9d540101f61e22a9"});
+**/
